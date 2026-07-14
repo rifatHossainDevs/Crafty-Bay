248f818e70ff65068ae9ebf5d8130a693baa7b54
Rifat Hossain <mdrifathossain472@gmail.com>
2026-07-15 00:52:37 +0600

    initial fvm, setup firebase, crashlytics, analytics

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+
+// Initialization FVM -> Done
+// Folder structure
+// Firebase Set up -> Done
+// Crashlytics -> Done
+// Analytics -> Done
+// Theming
+// Localization
+// Provider
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
